46d0d50395c98a3b526fb9883d7f3d0f141c31f2
dcassler <dannycassler@gmail.com>
2020-08-28 19:40:52 -0400

    Converted image downloader to function with call

--- scrape.py
+++ scrape.py
@@ -22,59 +22,59 @@ result = BeautifulSoup(response.text, "html.parser")
 
 
 
+def grabPicturesFromItem(result):
+    # Grab all images in carousel
+    pictureGroup = result.find('ul', attrs={'data-tn':'carousel-list-wrapper'})
+    parsedPictureGroup = pictureGroup.contents
 
-# Grab all images in carousel
-pictureGroup = result.find('ul', attrs={'data-tn':'carousel-list-wrapper'})
-parsedPictureGroup = pictureGroup.contents
+    # Parse pictures to array
+    i = 0
+    picList = [] # Array of all pictures
+    while i < len(parsedPictureGroup):
+        temp = str(pictureGroup.contents[i].find('img'))
+        if temp != 'None':
+            x = temp.split('src="')
+            picURL = x[1].replace('"/>', '')
+            #print(picURL)
+            #print(i, '-0--------')
+            picList.append(picURL)
+        i += 1
 
-# Parse pictures to array
-i = 0
-picList = [] # Array of all pictures
-while i < len(parsedPictureGroup):
-    temp = str(pictureGroup.contents[i].find('img'))
-    if temp != 'None':
-        x = temp.split('src="')
-        picURL = x[1].replace('"/>', '')
-        #print(picURL)
-        #print(i, '-0--------')
-        picList.append(picURL)
-    i += 1
+    # Fix first entry to grab largest picture
+    parsedOutput = picList[0].split(', ')
 
-# Fix first entry to grab largest picture
-parsedOutput = picList[0].split(', ')
+    fixedFirst = parsedOutput[len(parsedOutput) - 1].split(' ')
+    finalFirst = fixedFirst[0]
 
-fixedFirst = parsedOutput[len(parsedOutput) - 1].split(' ')
-finalFirst = fixedFirst[0]
+    picList[0] = finalFirst
 
-picList[0] = finalFirst
+    # Download all of the images
 
-# Download all of the images
-
-pathname = 'images'
-if not os.path.isdir(pathname):
-        os.makedirs(pathname)
-for image_url in picList:
-    filename = image_url.split("/")[-1] 
-    filename = filename.split("?")[0]
-    filename = filename.split(".jpg")[0]
-    filename = filename.split(".JPG")[0]
-    filename += ".jpeg"
-    filename = os.path.join(pathname, filename)
-    r = requests.get(image_url, stream = True)
-    file_size = int(response.headers.get("Content-Length", 0))
-     # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
-    progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
-    # Check if the image was retrieved successfully
-    if r.status_code == 200:
-    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-        r.raw.decode_content = True
-    # Open a local file with wb ( write binary ) permission.
-        with open(filename,'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-        
-        print('Image sucessfully Downloaded: ',filename)
-    else:
-        print('Image Couldn\'t be retreived')
+    pathname = 'images'
+    if not os.path.isdir(pathname):
+            os.makedirs(pathname)
+    for image_url in picList:
+        filename = image_url.split("/")[-1] 
+        filename = filename.split("?")[0]
+        filename = filename.split(".jpg")[0]
+        filename = filename.split(".JPG")[0]
+        filename += ".jpeg"
+        filename = os.path.join(pathname, filename)
+        r = requests.get(image_url, stream = True)
+        file_size = int(response.headers.get("Content-Length", 0))
+        # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
+        progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+        # Check if the image was retrieved successfully
+        if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            r.raw.decode_content = True
+        # Open a local file with wb ( write binary ) permission.
+            with open(filename,'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+            
+            print('Image sucessfully Downloaded: ',filename)
+        else:
+            print('Image Couldn\'t be retreived')
 
 # Grab Item Details
 def grabItemDetails(result):
@@ -104,8 +104,7 @@ def grabDepth(dimpullOutData):
     dimDepth = tempDimDepth.replace('</', '')
     return dimDepth
 
-# Grab item Dimensions
-grabItemDetails(result)
+
 
 
 # About Section: Holds data from the about section to store in each item descripton
@@ -129,3 +128,12 @@ setSizePullOutData = str(setSizeDetailGroup)
 setSizePullOutData = setSizePullOutData.split('>')
 setSizeData = setSizePullOutData[2].replace('</span', '')
 #print(setSizeData)
+
+
+# ----------- Function Calls ----------- #
+
+
+#grabPicturesFromItem(result)
+
+#grabItemDimensions(result)
+
