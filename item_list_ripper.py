58238b36551d1e283c8f945d090c722ff3b44a1c
dcassler <dannycassler@gmail.com>
2020-08-29 21:26:25 -0400

    Make it all function in a list. Tracks item number

--- item_list_ripper.py
+++ item_list_ripper.py
@@ -1,1 +1,46 @@
 import download_item_data
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+# Store page url
+# https://www.1stdibs.com/dealers/mid-century-antiques/shop/furniture/
+
+
+# search for: application/ld+json
+
+def grabStoreResults(addition):
+
+    url = 'https://www.1stdibs.com/dealers/mid-century-antiques/shop/furniture/' + addition
+    response = requests.get(url)
+    result = BeautifulSoup(response.text, "html.parser")
+    return condenseResult(result)
+
+def condenseResult(result):
+    neededChunk = result.find('script', attrs={'type':'application/ld+json'})
+    neededChunk = str(neededChunk)
+    parsedChunkByURL = neededChunk.split('url":"')
+    itemPages = []
+    for url in parsedChunkByURL:
+        url = url.split('"')
+        #print('url', url[0])
+        itemPages.append(url[0])
+    del itemPages[0]
+    
+    return itemPages
+
+def downloadItems():
+    pages = ['', '?page=2', '?page=3', '?page=4', '?page=5']
+    i = 0
+    for page in pages: 
+        result = grabStoreResults(page)
+        del result[0]
+        
+        print(result)
+        for item in result: 
+            print("Attempting donwload of ", item, '\n')
+            print('This is item ', i)
+            print(download_item_data.grabData(item))
+            i += 1
+
+downloadItems()
