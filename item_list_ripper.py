01ebec632f569e96cb87ffd3e8f2ab97eb599fd5
Dan Cassler <dan@t440p.home>
2020-09-01 22:29:42 -0400

    Connection is made and can execute commands to server

--- item_list_ripper.py
+++ item_list_ripper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import download_item_data
+import db_connect
 # Store page url
 # https://www.1stdibs.com/dealers/mid-century-antiques/shop/furniture/
 
@@ -28,6 +29,11 @@ def condenseResult(result):
     return itemPages
 
 def downloadItems():
+    db = db_connect.connection()
+    con = db.cursor()
+    #con.execute("select * from Item;")
+    #x = con.fetchall()
+    #print(x)
     pages = ['', '?page=2', '?page=3', '?page=4', '?page=5']
     i = 0
     for page in pages: 
@@ -38,6 +44,25 @@ def downloadItems():
         for item in result: 
             print("Attempting donwload of ", item, '\n')
             print('This is item ', i)
-            print(download_item_data.grabData(item))
+            data = download_item_data.grabData(item)
+            # Parse out that data into variables to send to database            
+            height = data[0][0]
+            width = data[0][1]
+            depth = data[0][2]
+            seatHeight = data[0][3]
+            about = data[1]
+            price = data[2]
+            setsize = data[3]
+            pictureList = data[4]
+            print(height, '\n', width, '\n', depth, '\n', about, '\n')
+            name = "null"
+            itemtype = "null"
+           
+            #stmt = """INSERT INTO `Item` (`itemID`, `name`, `price`, `height`, `width`, `depth`, `about`, `setsize`, `pictureList`, `itemtype`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" % (None, name, price, height, width, depth, about, setsize, pictureList, itemtype)
+            stmt = "INSERT INTO `Item` (`itemID`, `name`, `price`, `height`, `width`, `depth`, `about`, `setsize`, `pictureList`, `itemtype`) VALUES (0, 'Did this actually work?',0,0,0,0,0,0,0,0);"
+            con.execute(stmt)
+            db.commit() 
+            print(data)
             i += 1
 
+downloadItems()
